5c833a6177ea0949cfd2c2b5d71adaa94d95c307
yanjan1 <yanjankaf@gmail.com>
2025-05-01 20:21:27 +0530

    Migration added

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return route('login');
 });
+
+
+Route::get('/login', function () {
+    return view('auth.login');
+})->name('login');
